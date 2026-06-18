bf43be2d95b335d0c5416cfae323abe848ff5655
jonasr1 <yatosama924@gmail.com>
2026-06-18 08:38:09 -0300

    feat: add motivational quotes and improve quote rotation

--- lib/features/home/data/quotes_data.dart
+++ lib/features/home/data/quotes_data.dart
@@ -7,8 +7,9 @@ const List<Quote> quotes = [
   ),
   Quote(text: 'Consistência supera intensidade.', author: 'Angela Duckworth'),
   Quote(
-    text: 'Pequenos passos todos os dias constroem grandes resultados.',
-    author: 'FlashMind',
+    text:
+        'Pequenos passos todos os dias constroem grandes mudanças para a vida inteira.',
+    author: 'Leonardo Bonvecchio',
   ),
   Quote(text: 'Aprender nunca esgota a mente.', author: 'Leonardo da Vinci'),
   Quote(
@@ -22,10 +23,269 @@ const List<Quote> quotes = [
     author: 'Jim Ryun',
   ),
   Quote(text: 'Grandes conquistas exigem tempo.', author: 'Maya Angelou'),
+
+  // Frases motivacionais curtas
+  Quote(
+    text:
+        'Às vezes, a vida vai te acertar um tijolo na cabeça. Não perca a fé.',
+    author: 'Steve Jobs, Apple',
+  ),
+  Quote(
+    text:
+        'Quando você acredita em algo, acredite até o fim. Não dê margem para nenhum tipo de dúvida.',
+    author: 'Walt Disney',
+  ),
+  Quote(
+    text:
+        'O entusiasmo é a maior força da alma. Conserva-o e nunca te faltará poder para conseguires o que desejas.',
+    author: 'Napoleon Hill',
+  ),
+  Quote(
+    text:
+        'No meio da confusão, encontre a simplicidade. A partir da discórdia, encontre a harmonia. No meio da dificuldade reside a oportunidade.',
+    author: 'Albert Einstein',
+  ),
+  Quote(text: 'Uma ideia é um feito de associação.', author: 'Robert Frost'),
+  Quote(
+    text:
+        'A sua vida é 10% do que acontece contigo e 90% de como você reage a isso.',
+    author: 'Charles Swindoll',
+  ),
+  Quote(
+    text: 'Nós nos tornamos aquilo que pensamos.',
+    author: 'Earl Nightingale, pioneiro em desenvolvimento pessoal',
+  ),
+  Quote(
+    text: 'Seja inteligente, mas nunca mostre demais.',
+    author: 'Louis B. Mayer, MGM',
+  ),
+  Quote(
+    text:
+        'Apenas um pequeno pensamento positivo pela manhã pode mudar todo o seu dia.',
+    author: 'Dalai Lama',
+  ),
+  Quote(
+    text: 'As oportunidades não acontecem, você as cria.',
+    author: 'Chris Grosser',
+  ),
+  Quote(
+    text: 'Ame sua família, trabalhe muito, viva sua paixão.',
+    author: 'Gary Vaynerchuk',
+  ),
+  Quote(
+    text: 'Nunca é tarde demais para ser o que você poderia ter sido.',
+    author: 'George Eliot',
+  ),
+  Quote(
+    text:
+        'Não deixe a opinião de outra pessoa sobre você se tornar sua realidade.',
+    author: 'Les Brown, palestrante motivacional e ex-político americano',
+  ),
+  Quote(
+    text: 'Se você não é energia positiva, você é energia negativa.',
+    author: 'Mark Cuban',
+  ),
+  Quote(
+    text:
+        'Eu não sou produto das minhas circunstâncias. Eu sou produto das minhas decisões.',
+    author: 'Stephen R. Covey',
+  ),
+  Quote(
+    text: 'Faça o melhor que puder. Ninguém pode fazer mais que isso.',
+    author: 'John Wooden',
+  ),
+  Quote(text: 'Se você pode sonhar, você pode fazer.', author: 'Walt Disney'),
+  Quote(
+    text: 'Faça o que puder, com o que tem, onde estiver.',
+    author: 'Theodore Roosevelt',
+  ),
+
+  // Frases motivacionais diárias
+  Quote(
+    text:
+        'A vida pode ser muito mais ampla uma vez que você descobre um fato simples: tudo ao seu redor que você chama de vida foi criado por pessoas que não eram mais inteligentes que você. E você pode mudá-la, você pode influenciá-la. ... Uma vez que você aprende isso, você nunca mais será o mesmo.',
+    author: 'Steve Jobs',
+  ),
+  Quote(
+    text:
+        'A vida é como andar de bicicleta. Para manter o equilíbrio, você deve continuar se movendo.',
+    author: 'Albert Einstein',
+  ),
+  Quote(
+    text: 'O que você faz fala tão alto que não consigo ouvir o que você diz.',
+    author: 'Ralph Waldo Emerson',
+  ),
+  Quote(
+    text:
+        'Eu nunca deixei minha educação escolar interferir na minha educação.',
+    author: 'Mark Twain',
+  ),
+  Quote(
+    text:
+        'Se você ainda não pode fazer grandes coisas, faça pequenas coisas de uma grande maneira.',
+    author: 'Napoleon Hill',
+  ),
+  Quote(
+    text:
+        'Se você realmente quer fazer algo, encontrará um jeito. Se não quiser, encontrará uma desculpa.',
+    author: 'Jim Rohn',
+  ),
+  Quote(
+    text:
+        'Certifique-se de colocar seus pés no lugar certo, então permaneça firme.',
+    author: 'Abraham Lincoln',
+  ),
+  Quote(
+    text: 'Viva da sua imaginação, não da sua história.',
+    author: 'Stephen Covey',
+  ),
+  Quote(
+    text:
+        'Não espere pelo momento e lugar perfeitos para entrar, pois você já está no palco.',
+    author: 'Desconhecido',
+  ),
+  Quote(
+    text: 'Quanto maior a dificuldade, maior a glória em superá-la.',
+    author: 'Epicuro',
+  ),
+  Quote(
+    text:
+        "A coragem nem sempre ruge. Às vezes a coragem é uma voz silenciosa no final do dia dizendo: 'Tentarei novamente amanhã'.",
+    author: 'Mary Anne Radmacher',
+  ),
+  Quote(
+    text:
+        'Se as decisões que você toma sobre onde investe seu sangue, suor e lágrimas não são consistentes com a pessoa que aspira ser, você nunca se tornará essa pessoa.',
+    author: 'Clayton M. Christensen',
+  ),
+
+  // Frases motivacionais poderosas
+  Quote(
+    text:
+        'A maneira mais comum de as pessoas desistirem de seu poder é pensando que não têm nenhum.',
+    author: 'Alice Walker',
+  ),
+  Quote(
+    text: 'O poder não é dado a você. Você tem que tomá-lo.',
+    author: 'Beyoncé',
+  ),
+  Quote(
+    text: 'Acredite que você pode e você já está na metade do caminho.',
+    author: 'Theodore Roosevelt',
+  ),
+  Quote(
+    text:
+        'Você tem poder sobre sua mente; não sobre eventos externos. Perceba isso, e você encontrará força.',
+    author: 'Marcus Aurelius',
+  ),
+  Quote(
+    text: 'O futuro pertence àqueles que acreditam na beleza de seus sonhos.',
+    author: 'Eleanor Roosevelt',
+  ),
+  Quote(
+    text:
+        'Não vá onde o caminho pode levar, vá em vez disso onde não há caminho e deixe uma trilha.',
+    author: 'Ralph Waldo Emerson',
+  ),
+  Quote(
+    text:
+        'A maior arma contra o estresse é nossa capacidade de escolher um pensamento sobre outro.',
+    author: 'William James',
+  ),
+  Quote(
+    text: 'Você deve ser a mudança que deseja ver no mundo.',
+    author: 'Mahatma Gandhi',
+  ),
+  Quote(
+    text: 'Não é se você cai; é se você se levanta.',
+    author: 'Vince Lombardi',
+  ),
+  Quote(
+    text: 'Com grandes poderes vêm grandes responsabilidades.',
+    author: 'Stan Lee',
+  ),
+
+  // Frases motivacionais profundas
+  Quote(
+    text:
+        'Não podemos resolver problemas com o tipo de pensamento que empregamos quando os criamos.',
+    author: 'Albert Einstein',
+  ),
+  Quote(
+    text:
+        'Aprenda como se fosse viver para sempre, viva como se fosse morrer amanhã.',
+    author: 'Mahatma Gandhi',
+  ),
+  Quote(
+    text:
+        'Fique longe daquelas pessoas que tentam menosprezar suas ambições. Mentes pequenas sempre farão isso, mas grandes mentes darão a você a sensação de que você também pode se tornar grande.',
+    author: 'Mark Twain',
+  ),
+  Quote(
+    text:
+        'Quando você dá alegria a outras pessoas, você recebe mais alegria em troca. Você deveria dar um bom pensamento à felicidade que pode espalhar.',
+    author: 'Eleanor Roosevelt',
+  ),
+  Quote(
+    text:
+        'Quando você muda seus pensamentos, lembre-se de também mudar seu mundo.',
+    author: 'Norman Vincent Peale',
+  ),
+  Quote(
+    text:
+        'É apenas quando corremos riscos que nossas vidas melhoram. O risco inicial e mais difícil que precisamos correr é nos tornarmos honestos.',
+    author: 'Walter Anderson',
+  ),
+  Quote(
+    text:
+        'A natureza nos deu todas as peças necessárias para alcançar bem-estar e saúde excepcionais, mas deixou para nós juntarmos essas peças.',
+    author: 'Diane McLaren',
+  ),
+
+  // Frases motivacionais engraçadas
+  Quote(
+    text:
+        'O elevador para o sucesso está fora de serviço. Você terá que usar as escadas, um degrau de cada vez.',
+    author: 'Joe Girard',
+  ),
+  Quote(
+    text:
+        'Seja um trampolim de energia positiva; absorva o que você precisa e rebata mais de volta.',
+    author: 'Dave Carolan',
+  ),
+  Quote(
+    text:
+        'As pessoas frequentemente dizem que a motivação não dura. Bem, nem o banho — é por isso que recomendamos diariamente.',
+    author: 'Zig Ziglar',
+  ),
+  Quote(
+    text: 'Trabalhe até sua conta bancária parecer um número de telefone.',
+    author: 'Desconhecido',
+  ),
+  Quote(
+    text:
+        'Eu sou tão inteligente que às vezes não entendo uma única palavra do que estou dizendo.',
+    author: 'Oscar Wilde',
+  ),
+  Quote(
+    text:
+        'As pessoas dizem que nada é impossível, mas eu não faço nada todos os dias.',
+    author: 'Ursinho Pooh',
+  ),
+  Quote(
+    text:
+        'A vida é como um esgoto... o que você tira dela depende do que você coloca nela.',
+    author: 'Tom Lehrer',
+  ),
+  Quote(
+    text:
+        'Eu sempre quis ser alguém, mas agora percebo que deveria ter sido mais específica.',
+    author: 'Lily Tomlin',
+  ),
 ];
 
 Quote getDailyQuote() {
-  final day = DateTime.now().day;
-
-  return quotes[day % quotes.length];
+  final now = DateTime.now();
+  final dayOfYear = now.difference(DateTime(now.year, 1, 1)).inDays;
+  return quotes[dayOfYear % quotes.length];
 }
